6d6b0c52c9c1c55b879d2603882968c1ebefa561
OmiSeerDev <omarmtinajero@gmail.com>
2024-01-11 21:49:46 -0600

    Format

--- CSPOO/Program.cs
+++ CSPOO/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 
-internal class Program {
+internal class Program
+{
 
-    enum Categories {
+    enum Categories
+    {
         Undefined,
         Analgesics,
         Antibiotics,
@@ -11,7 +13,8 @@ internal class Program {
         Materials
     }
 
-    class Product {
+    class Product
+    {
         string ProductName;
         Categories Category;
         string Presentation;
@@ -21,18 +24,20 @@ internal class Program {
         string Unit;
         public static string Legend = " Consulte a su médico";
 
-        public Product(string productName, string presentation, int grams, string unit, Categories category, int price, string description) {
+        public Product(string productName, string presentation, int grams, string unit, Categories category, int price, string description)
+        {
             ProductName = productName;
             Grams = grams;
             Unit = unit;
             Presentation = presentation.ToLower();
             Category = category;
-            Price = price/100;
+            Price = price / 100;
             Description = String.Concat(description, Legend);
         }
 
- 
-        public void getInfo() {
+
+        public void getInfo()
+        {
             Console.WriteLine(
                 $"Name: {ProductName}\nPresentation: {Grams}{Unit} {Presentation}\nCategory: {Category}\nPrice: {Convert.ToDouble(Price).ToString("C")}\nDescription: {Description}\n"
                 );
@@ -41,7 +46,8 @@ internal class Program {
 
 
 
-    private static void Main(string[] args) {
+    private static void Main(string[] args)
+    {
         Product paracetamol = new Product("Paracetamol", "TABLeTs", 500, "mg", Categories.Analgesics, 700, $"Efectivo contra el dolor leve y la fiebre.");
         paracetamol.getInfo();
     }
