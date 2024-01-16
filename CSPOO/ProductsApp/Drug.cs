2afa12fa48dadc9ecadd9b30f4fb1e62451b82e0
OmiSeerDev <omarmtinajero@gmail.com>
2024-01-15 23:46:16 -0600

    Cleaning code

--- CSPOO/ProductsApp/Drug.cs
+++ CSPOO/ProductsApp/Drug.cs
@@ -1,8 +1,10 @@
 ﻿using System.Text;
 using CSPOO.Interfaces;
 
-namespace CSPOO.ProductsApp {
-    public class Drug : PharmacyStock, IProduct {
+namespace CSPOO.ProductsApp
+{
+    public class Drug : PharmacyStock, IProduct
+    {
         public string ProductName { get; set; }
         public int Price { get; set; }
         public static string Legend = " Consulte a su médico";
@@ -13,30 +15,35 @@ namespace CSPOO.ProductsApp {
 
 
 
-        public Drug(string productName, int price, string description, string presentation, int grams, string unit, Categories[] category) {
+        public Drug(string productName, int price, string description, string presentation, int grams, string unit, Categories[] category)
+        {
             ProductName = productName;
             Price = price / 100;
             Description = string.Concat(description, Legend);
-           
+
             Grams = grams;
             Unit = unit;
             Presentation = presentation.ToLower();
-            foreach (var element  in category) {
+            foreach (var element in category)
+            {
                 Category.Add(element);
             }
         }
 
 
-        public string catList() {
+        public string catList()
+        {
             StringBuilder res = new();
-            foreach (var el in Category) {
+            foreach (var el in Category)
+            {
                 res.Append($"{Convert.ToString(el)}, ");
             }
-             return res.ToString().Substring(0, res.Length - 2); //Removes the last comma and space
+            return res.ToString().Substring(0, res.Length - 2); //Removes the last comma and space
         }
 
 
-        public void getInfo() {
+        public void getInfo()
+        {
             string cats = catList();
             Console.WriteLine(
                 $"Name: {ProductName}\n" +
--- CSPOO/Program.cs
+++ CSPOO/Program.cs
@@ -1,12 +1,11 @@
-﻿using System.Text;
-using CSPOO.ProductsApp;
+﻿using CSPOO.ProductsApp;
 
 internal class Program
 {
     private static void Main()
     {
         Categories[] cats = { (Categories)1, Categories.Antipyrethics };
-        Drug paracetamol = new("Paracetamol", 1300, "Efectivo contra el dolor leve y la fiebre.", "TABLeTs",500, "mg", cats);
+        Drug paracetamol = new("Paracetamol", 1300, "Efectivo contra el dolor leve y la fiebre.", "TABLeTs", 500, "mg", cats);
         paracetamol.getInfo();
     }
 
